25950839b89639079f91d4ec0bb33619f539d8dc
szoker <szoker527@gmail.com>
2021-12-02 19:07:56 +0100

    Working on functions

--- script.js
+++ script.js
@@ -7,63 +7,107 @@ const divKey = document.querySelector("#divide");
 const equalKey = document.querySelector("#equals");
 const zeroKey = document.querySelector("#zerobtn button");
 const display = document.querySelector("#shownumbers");
+let adds = "+";
+let subtracts = "-";
+let multiplys = "*";
+let divides = "/";
 let choice;
-let displayNumbers = {};
+let disNum = {};
 
 
 keyClear.addEventListener("click", clearInput);
 keys.forEach(key => key.addEventListener("click", inputNumbers));
 zeroKey.addEventListener("click", inputNumbers);
 
-addKeys.addEventListener("click", numberOne);
 addKeys.addEventListener("click", futureEqual);
+addKeys.addEventListener("click", numberOne);
 
-subKeys.addEventListener("click", numberOne);
 subKeys.addEventListener("click", futureEqual);
+subKeys.addEventListener("click", numberOne);
 
-multKeys.addEventListener("click", numberOne);
 multKeys.addEventListener("click", futureEqual);
+multKeys.addEventListener("click", numberOne);
 
-divKey.addEventListener("click", numberOne);
 divKey.addEventListener("click", futureEqual);
+divKey.addEventListener("click", numberOne);
 
 equalKey.addEventListener("click", finalEqual);
 
 
 function add(a, b){
-    return a + b;
+    if(disNum.score === undefined) {
+        disNum.finalEq = a + b;
+    }
+    else {
+        disNum.finalEq += a;
+        display.textContent = disNum.finalEq;
+    }
+
 }
 
 function subtract(a, b) {
-    return a - b;
+    disNum.score = a - b;
+    display.textContent = disNum.score;
+    display.textContent = String(display.textContent).substr(0, 12);
+    return disNum.score;
 }
 
 function multiply(a, b) {
-    return a * b;
+    disNum.score = a * b;
+    display.textContent = disNum.score;
+    display.textContent = String(display.textContent).substr(0, 12);
+    return disNum.score;
 }
 
 function divide(a, b) {
-    return a / b;
+    disNum.score = a / b;
+    display.textContent = disNum.score;
+    display.textContent = String(display.textContent).substr(0, 12);
+    return disNum.score;
 }
 
 function operator(choice) {
-    let a = displayNumbers.firstNumber
-    let b = displayNumbers.secondNumber
-    switch (choice) {
-        case "add":
-            display.textContent = add(+a, +b);
-            return add(+a, +b);
-        case "subtract":
-            display.textContent = subtract(+a, +b);
-            return subtract(+a, +b);
-        case "multiply":
-            display.textContent = multiply(+a, +b);
-            return multiply(+a, +b);
-        case "divide":
-            display.textContent = divide(+a, +b);
-            return divide(+a, +b);
+    let a;
+    let b;
+    if(disNum.score === undefined) {
+        a = disNum.fNum;
+        b = disNum.sNum;
+    }
+    else if(disNum.sNum === undefined){ 
+        a = disNum.score;
+        b = disNum.fNum;
+    }
+    else {
+        a = disNum.score;
+        b = disNum.sNum;
+    }
+    if (isNaN(a)) {
+        a = 0;
+    }
+    else if (isNaN(b)) {
+        b = a;
+    }
+    if (choice == "add") {
+        console.log(a,b);
+        add(a, b);
+    }
+    else if(choice == "subtract") {
+        console.log(a,b);
+        subtract(+a, +b);
+        disNum.finalScore = +display.textContent;
+    }
+    else if(choice == "multiply"){
+        console.log(a,b);
+        multiply(+a, +b);
+        disNum.finalScore = +display.textContent;
+    }
+    else if(choice == "divide"){
+        console.log(a,b);
+        divide(+a, +b);
+        disNum.finalScore = +display.textContent;
     }
 }
+
 
 function futureEqual(e){
     choice = e.target.id;
@@ -72,8 +116,11 @@ function futureEqual(e){
 function inputNumbers(e) {
     const choice = e.target.id;
     const display = document.querySelector("#shownumbers");
-    if (display.textContent == 0){
+    display.textContent = String(display.textContent).substr(0, 12);
+    if (display.textContent == 0 || display.textContent == "+" || display.textContent == "-" ||
+    display.textContent == "*" || display.textContent == "/" || !isNaN(disNum.finalScore)){
         display.textContent = choice;
+        delete disNum.finalScore;
     }
     else {
         display.textContent += choice;
@@ -82,16 +129,78 @@ function inputNumbers(e) {
 
 function clearInput() {
     const display = document.querySelector("#shownumbers");
+    delete disNum.fNum;
+    delete disNum.sNum;
+    delete disNum.finalScore;
     display.textContent = "0";
 }
 
 function numberOne(e) {
-    displayNumbers.firstNumber = +display.innerText;
-    display.innerText = e.target.id;
+    isNumber();
+    showChoice(choice)
+    console.log(disNum.fNum); console.log(disNum.sNum); console.log (disNum.score);
 }
 
+function isNumber() {
+    if (disNum.fNum === undefined){
+        disNum.fNum = +display.innerText;
+    }
+    else if(!isNaN(disNum.fNum && disNum.sNum) || disNum.sNum === undefined){
+        disNum.sNum = +display.innerText;
+        if (choice == "add") {
+            if(disNum.score === undefined) {
+                disNum.score = disNum.fNum + disNum.sNum;
+                delete disNum.sNum;
+            }
+            else {
+                disNum.score += disNum.sNum;
+            }
+        }
+        else if (choice == "subtract") {
+            if(disNum.score === undefined) {
+                disNum.score = disNum.fNum - disNum.sNum;
+                delete disNum.sNum;
+            }
+            else {
+                disNum.score -= disNum.sNum;
+            }
+        }
+        else if (choice == "multiply") {
+            if(disNum.score === undefined) {
+                disNum.score = disNum.fNum * disNum.sNum;
+                delete disNum.sNum;
+            }
+            else {
+                disNum.score *= disNum.sNum;
+            }
+        }
+        else if (choice == "divide") {
+            if(disNum.score === undefined) {
+                disNum.score = disNum.fNum / disNum.sNum;
+                delete disNum.sNum;
+            }
+            else {
+                disNum.score /= disNum.sNum;
+            }
+        }
+    }  
+}
+
+function showChoice(choice) {
+    switch (choice) {
+        case "add":
+           return display.textContent = adds;
+        case "subtract":
+            return display.textContent = subtracts;
+        case "multiply":
+            return display.textContent = multiplys;
+        case "divide":
+            return display.textContent = divides;
+    }
+}
+
+
 function finalEqual() {
-    displayNumbers.secondNumber = +display.innerText;
     if (choice === undefined){
         display.innerText = "0";
     }
